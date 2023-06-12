5abf703cbcaecf153c1504a66e275809dd9b7fa8
Dongwon, Kim <68371880+comwitch@users.noreply.github.com>
2023-06-12 23:08:24 +0900

    finish part 2 with some bug

--- Vulkan_Tutorial/Vulkan_Tutorial/first_app.cpp
+++ Vulkan_Tutorial/Vulkan_Tutorial/first_app.cpp
@@ -1,6 +1,7 @@
 #include "first_app.h"
 
 #include <stdexcept>
+#include <array>
 
 namespace lve {
 	First_app::First_app()
@@ -17,7 +18,10 @@ namespace lve {
 		while (!LveWindow.shouldClose())
 		{
 			glfwPollEvents();
+			drawFrame();
 		}
+
+		vkDeviceWaitIdle(LveDevice.device());
 
 
 	}
@@ -49,9 +53,72 @@ namespace lve {
 	}
 	void First_app::createCommandBuffers()
 	{
+		commandBuffers.resize(lveSwapChain.imageCount());
 
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.commandPool = LveDevice.getCommandPool();
+		allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+		if (vkAllocateCommandBuffers(LveDevice.device(), &allocInfo, commandBuffers.data()) !=
+			VK_SUCCESS)
+		{
+			throw std::runtime_error("tunatuna fail to allocate command buffers!");
+		}
+
+		for (int i = 0; i < commandBuffers.size(); i++)
+		{
+			VkCommandBufferBeginInfo beginInfo{};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+			if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS)
+			{
+				throw std::runtime_error("tunatuna failed to begin recording command buffer!");
+			}
+
+			VkRenderPassBeginInfo renderPassInfo{};
+			renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+			renderPassInfo.renderPass = lveSwapChain.getRenderPass();
+			renderPassInfo.framebuffer = lveSwapChain.getFrameBuffer(i);
+
+			renderPassInfo.renderArea.offset = { 0,0 };
+			renderPassInfo.renderArea.extent = lveSwapChain.getSwapChainExtent();
+
+			std::array<VkClearValue, 2> clearValues{};
+			clearValues[0].color = { 0.1f,0.1f, 0.1f, 0.1f };
+			clearValues[1].depthStencil = { 1.0f, 0 };
+			renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+			renderPassInfo.pClearValues = clearValues.data();
+
+			vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+			lvePipeline->bind(commandBuffers[i]);
+			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+			vkCmdEndRenderPass(commandBuffers[i]);
+			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("tuna tuna failed to record command buffer!");
+			}
+
+		}
+
+		
 	}
 	void First_app::drawFrame()
 	{
+		uint32_t imageIndex;
+		auto result = lveSwapChain.acquireNextImage(&imageIndex);
+
+		if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
+		{
+			throw std::runtime_error("failed to acquire swapchain image!");
+		}
+
+		result = lveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+		if (result != VK_SUCCESS)
+		{
+			throw std::runtime_error("tunatuna failed to present swap chain image1");
+		}
 	}
 }
